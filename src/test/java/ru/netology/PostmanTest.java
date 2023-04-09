ffefe245cbaaca01aef519c61443889ebcc977cd
ShuranovaNI <ni.shuranova@gmail.com>
2023-04-09 20:07:58 +0300

    commit wich test fell

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class ApiTest {
+    @Test
+    void ApiTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(400)
+                .body("data",equalTo("some data"))
+        ;
+    }
+}
